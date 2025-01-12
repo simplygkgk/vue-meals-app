451cd896389cf71f4c9f6a4d1307e2b0c6e94997
Gaurav Kumar <kumargaurav1628@gmail.com>
2025-01-12 18:43:38 +0530

    filter added and slot fixed

--- src/components/YouTubeButton.vue
+++ src/components/YouTubeButton.vue
@@ -4,7 +4,7 @@
     target="_blank"
     class="px-3 py-2 cursor-pointer text-white rounded border-2 border-orange-600 bg-orange-500 hover:bg-orange-600 hover:text-white transition-colors"
     >
-    <slot></slot>
+    <slot>YouTube</slot>
     </a
   >
 </template>
